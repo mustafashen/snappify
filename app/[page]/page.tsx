00d2951f3c9c24f8da5f8dc5afa12b2712ea1eb1
mustafa sen <mustafasen@tuta.io>
2024-03-12 23:54:55 +0300

    visual changes

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -30,14 +30,13 @@ export async function generateMetadata({
 
 export default async function Page({ params }: { params: { page: string } }) {
   const page = await getPage(params.page);
-
   if (!page) return notFound();
 
   return (
     <>
-      <h1 className="mb-8 text-5xl font-bold">{page.title}</h1>
-      <Prose className="mb-8" html={page.body as string} />
-      <p className="text-sm italic">
+      <h1 className="">{page.title}</h1>
+      <Prose className="" html={page.body as string} />
+      <p className="">
         {`This document was last updated on ${new Intl.DateTimeFormat(undefined, {
           year: 'numeric',
           month: 'long',
--- app/error.tsx
+++ app/error.tsx
@@ -2,14 +2,14 @@
 
 export default function Error({ reset }: { reset: () => void }) {
   return (
-    <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12">
-      <h2 className="text-xl font-bold">Oh no!</h2>
-      <p className="my-2">
+    <div className="">
+      <h2 className="">Oh no!</h2>
+      <p className="">
         There was an issue with our storefront. This could be a temporary issue, please try your
         action again.
       </p>
       <button
-        className="mx-auto mt-4 flex w-full items-center justify-center rounded-full bg-blue-600 p-4 tracking-wide text-white hover:opacity-90"
+        className=""
         onClick={() => reset()}
       >
         Try Again
